27678585b25bca75cf712616fd774a8814bb9f39
R3l3ntl3ss <dev.91096@gmail.com>
2019-02-12 17:38:37 +0530

    Fixed Router Error

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { HashRouter as Router, Route } from "react-router-dom";
+// import createHashHistory from "history/createHashHistory";
+
+// const hashHistory = createHashHistory({ basename: process.env.PUBLIC_URL });
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Router basename={process.env.PUBLIC_URL}>
+          <React.Fragment>
+            <Route
+              exact
+              path="/"
+              render={props => (
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                  <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                  </p>
+                  <a
+                    className="App-link"
+                    href="#/about"
+                    rel="noopener noreferrer"
+                  >
+                    About
+                  </a>
+                </header>
+              )}
+            />
+
+            <Route
+              path="/about"
+              render={props => (
+                <React.Fragment>
+                  <h1>About</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Integer eleifend viverra rhoncus. Praesent at sapien ut ex
+                    lobortis interdum. Integer nec dapibus purus. Nulla
+                    facilisi. Curabitur euismod semper pulvinar. In consequat
+                    varius augue, eget aliquam erat tincidunt ac. Nullam dictum
+                    nisl sed sapien lobortis pharetra.
+                  </p>
+                </React.Fragment>
+              )}
+            />
+          </React.Fragment>
+        </Router>
       </div>
     );
   }
